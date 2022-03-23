3c35cd7dae13b798b5b3c77d2bae9da3d026f3ba
chliyang <chengli.yang@thoughtworks.com>
2022-03-23 23:32:05 +0800

    [flex] Vertical arrangement

--- src/components/flex-layout/FlexLayout.js
+++ src/components/flex-layout/FlexLayout.js
@@ -6,6 +6,10 @@ const FlexLayout = () => {
     <div className="flex-container">
       <Boxes title={"水平正序排列"} className={"layout1"} />
       <Boxes title={"水平逆序排列"} className={"layout2"} />
+      <div className="column">
+        <Boxes title={"垂直正序排列"} className={"layout3"} />
+        <Boxes title={"垂直逆序排列"} className={"layout4"} />
+      </div>
     </div>
   );
 };
