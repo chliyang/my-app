a8a47ea04bb9a1660cccbc0bd4805214dd03cfc9
chliyang <chengli.yang@thoughtworks.com>
2022-03-24 00:49:36 +0800

    [grid] create a 3x3 grid

--- src/components/grid-layout/GridLayout.js
+++ src/components/grid-layout/GridLayout.js
@@ -1,6 +1,20 @@
 import "./GridLayout.css";
 
 const GridLayout = () => {
-  return <div className="grid-container">grid</div>;
+  return (
+    <div className="grid-container">
+      <div className="layout-1">
+        <div className="box-1">1</div>
+        <div className="box-2">2</div>
+        <div className="box-3">3</div>
+        <div className="box-4">4</div>
+        <div className="box-5">5</div>
+        <div className="box-6">6</div>
+        <div className="box-7">7</div>
+        <div className="box-8">8</div>
+        <div className="box-9">9</div>
+      </div>
+    </div>
+  );
 };
 export default GridLayout;
