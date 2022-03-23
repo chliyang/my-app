8d1795136c4d111108a077e72d19642fa13bf6c1
chliyang <chengli.yang@thoughtworks.com>
2022-03-24 00:55:35 +0800

    [grid] extract GridBoxes component

---
+++ src/components/grid-layout/components/GridBoxes.js
@@ -0,0 +1,17 @@
+const GridBoxes = (props) => {
+  return (
+    <div className={props.className}>
+      <div className="box-1">1</div>
+      <div className="box-2">2</div>
+      <div className="box-3">3</div>
+      <div className="box-4">4</div>
+      <div className="box-5">5</div>
+      <div className="box-6">6</div>
+      <div className="box-7">7</div>
+      <div className="box-8">8</div>
+      <div className="box-9">9</div>
+    </div>
+  );
+};
+
+export default GridBoxes;
