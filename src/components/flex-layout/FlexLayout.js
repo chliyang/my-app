6c499c5adea3725a533e42c66dede2015c83a314
chliyang <chengli.yang@thoughtworks.com>
2022-03-30 00:37:34 +0800

    [CSS Module] resolve CSS global pollution problem of flex

--- src/components/flex-layout/FlexLayout.js
+++ src/components/flex-layout/FlexLayout.js
@@ -1,22 +1,22 @@
-import "./FlexLayout.module.css";
+import styles from "./FlexLayout.module.scss";
 import Boxes from "./components/Boxes";
 
 const FlexLayout = () => {
   return (
-    <div className="flex-container">
-      <Boxes title={"水平正序排列"} className={"layout1"} />
-      <Boxes title={"水平逆序排列"} className={"layout2"} />
-      <div className="column">
-        <Boxes title={"垂直正序排列"} className={"layout3"} />
-        <Boxes title={"垂直逆序排列"} className={"layout4"} />
+    <div className={styles.container}>
+      <Boxes title={"水平正序排列"} className={styles.layout} />
+      <Boxes title={"水平逆序排列"} className={styles.layout2} />
+      <div className={styles.column}>
+        <Boxes title={"垂直正序排列"} className={styles.layout3} />
+        <Boxes title={"垂直逆序排列"} className={styles.layout4} />
       </div>
       <>
-        <div className="title">单个项目变化</div>
-        <div className="layout1">
-          <section className="box">1</section>
-          <section className="box2">2</section>
-          <section className="box3">3</section>
-          <section className="box4">4</section>
+        <div className={styles.title}>单个项目变化</div>
+        <div className={styles.layout}>
+          <section className={styles.box}>1</section>
+          <section className={styles.box2}>2</section>
+          <section className={styles.box3}>3</section>
+          <section className={styles.box4}>4</section>
         </div>
       </>
     </div>
--- src/components/flex-layout/components/Boxes.js
+++ src/components/flex-layout/components/Boxes.js
@@ -1,16 +1,18 @@
+import styles from "../FlexLayout.module.scss";
+
 const Boxes = (props) => {
   return (
     <>
-      <div className="title">{props.title}</div>
+      <div className={styles.title}>{props.title}</div>
       <div className={props.className}>
-        <section className="box">1</section>
-        <section className="box">2</section>
-        <section className="box">4</section>
-        <section className="box">5</section>
-        <section className="box">6</section>
-        <section className="box">7</section>
-        <section className="box">8</section>
-        <section className="box">9</section>
+        <section className={styles.box}>1</section>
+        <section className={styles.box}>2</section>
+        <section className={styles.box}>4</section>
+        <section className={styles.box}>5</section>
+        <section className={styles.box}>6</section>
+        <section className={styles.box}>7</section>
+        <section className={styles.box}>8</section>
+        <section className={styles.box}>9</section>
       </div>
     </>
   );
